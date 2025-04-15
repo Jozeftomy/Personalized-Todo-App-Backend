865363669ad46114f06f6773f2fd8bc15b3e02a3
Jozeftomy <josephtomy02@gmail.com>
2025-04-15 17:07:37 +0530

    error fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
         const { name, email, password } = req.body;
         const existingUser = await User.findOne({ email });
 
-        if (existingUser) return res.status(400).json({ message: "Email already in use." });
+        if (existingUser) return res.status(400).json({ message: "Email already in use."});
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
